379682812dc94d46fc9b02c4ba1b26798d1df906
maxProvorov <maxiys1@rambler.ru>
2025-04-23 17:34:24 +0300

    add set webhook

---
+++ set_webhook.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+$config = require __DIR__.'/config/config.php';
+
+use App\Bot;
+
+$bot = new Bot($config['telegram']['bot_token']);
+$url = urlencode($config['telegram']['webhook_url']);
+$token = urlencode($config['telegram']['bot_token']);
+file_get_contents("https://api.telegram.org/bot{$token}/setWebhook?url=$url");
+echo "Webhook установлен!";
